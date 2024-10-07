76a43762c4f3ab723ac2e559e2a247fadc14d8e0
Nnange <awong.nnange@yahoo.com>
2024-10-07 17:32:45 +0200

    BE room delete func

--- nnangeHotel/src/main/java/com/nnangedev/nnangehotel/controller/RoomController.java
+++ nnangeHotel/src/main/java/com/nnangedev/nnangehotel/controller/RoomController.java
@@ -10,6 +10,7 @@ import com.nnangedev.nnangehotel.service.BookingService;
 import com.nnangedev.nnangehotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,6 +61,12 @@ public class RoomController {
             }
         }
         return ResponseEntity.ok(roomResponses);
+    }
+
+    @DeleteMapping("/delete/room/{roomId}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable Long roomId) {
+        roomService.deleteRoom(roomId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     private RoomResponse getRoomResponse(Room room){
