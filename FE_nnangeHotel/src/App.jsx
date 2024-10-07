9d79849b4e4adb6f2e6eea55f9af348c8a4e9683
Nnange <awong.nnange@yahoo.com>
2024-10-07 17:49:45 +0200

    bugfix for room delete

--- FE_nnangeHotel/src/App.jsx
+++ FE_nnangeHotel/src/App.jsx
@@ -1,19 +1,27 @@
-import AddRoom from './components/room/AddRoom'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import ExistingRooms from './components/room/ExistingRooms';
+import ExistingRooms from "./components/room/ExistingRooms";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditRoom from "./components/common/EditRoom";
+import Home from "./components/home/Home";
 
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 // import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 
 function App() {
-
   return (
     <>
-      <AddRoom/>
-      <ExistingRooms />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FE_nnangeHotel/src/components/common/EditRoom.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const EditRoom = () => {
+  return (
+    <div>
+        <h2>EditRoom</h2>
+    </div>
+  )
+}
+
+export default EditRoom
