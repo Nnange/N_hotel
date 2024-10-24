4934146c3771014b1b0f2e28dda9bb317c8b46e9
Nnange <awong.nnange@yahoo.com>
2024-10-24 21:00:11 +0200

    fix NavBar

--- FE_nnangeHotel/src/App.jsx
+++ FE_nnangeHotel/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EditRoom from "./components/common/EditRoom";
 import Home from "./components/home/Home";
 import AddRoom from "./components/room/AddRoom";
-import NavBar from "./components/layout/Navbar";
+import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
 
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
